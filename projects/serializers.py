14cc199bd099c494eaac614ca91313238d73060b
Laurent Jouron <jouronlaurent@hotmail.com>
2023-08-28 10:28:09 +0200

    need to see Choices

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,7 +1,14 @@
+from re import T
 from rest_framework import serializers
 
 from users.models import User
 from .models import Project, TypeChoice
+
+
+class TypeChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TypeChoice
+        fields = ('id', 'name')
 
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
@@ -10,11 +17,12 @@ class ProjectSerializer(serializers.HyperlinkedModelSerializer):
         many=True, read_only=True, view_name='issue-detail'
     )
     contributor = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(), many=True
+        queryset=User.objects.all(), many=False
     )
-    type_choice = serializers.PrimaryKeyRelatedField(
-        queryset=TypeChoice.objects.all(), many=False
-    )
+    # type_choice = serializers.PrimaryKeyRelatedField(
+    #     queryset=TypeChoice.objects.all()
+    # )
+    type_choice = TypeChoiceSerializer(many=False)
 
     class Meta:
         model = Project
@@ -36,13 +44,20 @@ class ProjectSerializer(serializers.HyperlinkedModelSerializer):
         return self.title
 
     def create(self, validated_data):
+        type_choices_data = validated_data.pop('type_choice')
         contributors_data = validated_data.pop('contributor', [])
         project = Project.objects.create(**validated_data)
         project.contributor.set(contributors_data)
+        for type_choice_data in type_choices_data:
+            TypeChoice.objects.create(project=project, **type_choice_data)
         return project
 
     def update(self, instance, validated_data):
         contributors_data = validated_data.pop('contributor', [])
+        type_choice = validated_data.pop('type_choice', None)
+        if type_choice:
+            type_choice = TypeChoice.objects.get(name=type_choice)
+            instance.type_choice = type_choice
         instance = super().update(instance, validated_data)
         instance.contributor.set(contributors_data)
         return instance
